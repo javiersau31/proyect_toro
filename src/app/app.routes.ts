5889c1d3bef3d11a557e66adff0d953201e851db
Javier Saucedo <javisaucedoperez6@gmail.com>
2025-07-05 12:49:15 -0600

    Vaciando main

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegistroComponent } from './registros/registros.component'; 
-
-export const appRoutes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'registro', loadComponent: () => import('./registros/registros.component').then(m => m.RegistroComponent) },
-  { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
-  {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
-];
