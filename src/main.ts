0cd3ed59c8f487c35aa95a0a181ff3d8afd0deb3
Javier Saucedo <javisaucedoperez6@gmail.com>
2025-07-05 12:36:21 -0600

    Subiendo carpeta

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { appRoutes } from './app/app.routes'; // <-- usa el nombre correcto
+import { appRoutes } from './app/app.routes'; 
 
 
 bootstrapApplication(AppComponent, {
